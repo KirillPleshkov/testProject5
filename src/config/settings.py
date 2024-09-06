edc71d38c3dc84e16902c015cfb615537038ee59
kirill <kirypleshkov@mail.ru>
2024-09-06 17:58:30 +0700

    Add 'rest_framework'

--- src/config/settings.py
+++ src/config/settings.py
@@ -19,6 +19,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Packets
+    "rest_framework",
     # Apps
     "src.apps.car",
     "src.apps.comment",

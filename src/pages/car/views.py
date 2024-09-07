77367762923a6f367a4e75f61e28ef3ef97c13a5
kirill <kirypleshkov@mail.ru>
2024-09-07 20:26:48 +0700

    Add home page with car list

--- src/pages/car/views.py
+++ src/pages/car/views.py
@@ -2,13 +2,16 @@ from typing import Any
 
 from django.views.generic import TemplateView
 
+from src.apps.car.models import Car
+
 
 class CarView(TemplateView):
     """View для отображения списка автомобилей"""
 
-    template_name = "car/car.html"
+    template_name = "car/cars.html"
 
     def get_context_data(self, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(id=id, **kwargs)
         context["title"] = "Главная"
+        context["cars"] = Car.objects.all()
         return context
